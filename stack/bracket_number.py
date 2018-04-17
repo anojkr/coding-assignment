335129bd546fa9608da235a36092328f79d8e51e
anojk <anoj.kumar48@gmail.com>
2018-04-17 05:39:52 +0530

    recheck code

--- stack/bracket_number.py
+++ stack/bracket_number.py
@@ -1,7 +1,6 @@
 #https://practice.geeksforgeeks.org/problems/print-bracket-number/0
 
 T = int(input())
-
 for _ in range(T):
 		N = input().split()
 		#print(N)
@@ -17,3 +16,19 @@ for _ in range(T):
 				l.append(stack.pop())
 		str1 = ' '.join(str(e) for e in l)
 		print(str1)
+
+"""
+Example:
+Input:
+3​
+( a + ( b * c ) ) + ( d / e )​
+( ( ( ) ) ( ( ) ) )
+( ( ( ( ( )
+Output:
+1 2 2 1 3 3
+1 2 3 3 2 4 5 5 4 1
+1 2 3 4 5 5
+
+Explanation:
+Test case 1:The highlighted brackets in the given expression (a+(b*c))+(d/e) has been assigned the numbers as: 1 2 2 1 3 3.
+"""
