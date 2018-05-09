369f72259bdcc49b89bfd139cd677445302fe7ce
anojk <anoj.kumar48@gmail.com>
2018-05-09 14:45:51 +0530

    Delete operation on BST refactor

--- tree_theory/tree.py
+++ tree_theory/tree.py
@@ -1,18 +1,19 @@
 class Node:
 
     def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+        self.data   = data
+        self.left   = None
+        self.right  = None
         self.parent = None
 
 
 class Binary_Search_Tree:
 
     def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+        self.data   = data
+        self.left   = None
+        self.right  = None
+        self.parent = None
 
     def insert_node(self, root, *newdata):
         for data in newdata:
@@ -58,38 +59,71 @@ class Binary_Search_Tree:
 
     def delete_node(self, root, data):
         node = self.search_node(root, data)
-        if node == None:
+        if node is None:
             print("Element not exist")
-        elif node.left == None and node.right == None:
-            if node.parent.data >= node.data:
-                node.parent.left = None
-            else:
-                node.parent.right = None
             return node
 
+        elif node.parent is not None:
+            #Delete of leaf node
+            if node.left == None and node.right == None:
+                if node.parent.data >= node.data:
+                    node.parent.left = None
+                else:
+                    node.parent.right = None
+
+            #Delete of node having two child
+            elif node.left!= None and node.right!=None:
+                t = self.inorder_min(node)
+                self.delete_node(t, t.data)
+                if node.parent.data >= node.data:
+                    node.parent.left = t
+                    t.left = node.left
+                    t.right = node.right
+                else:
+                    node.parent.right = t
+                    t.left = node.left
+                    t.right = node.right
+
+            #Delete node having one child
+            else:
+                t = self.inorder_min(node)
+                print(t.data)
+                if node.left is None and node.parent.data<=node.data:
+                    node.parent.right = t
+                elif node.right is None and node.parent.data>=node.data:
+                    node.parent.left = t
+
+        #Delete root node
         else:
             t = self.inorder_min(node)
             s = self.delete_node(t, t.data)
-            if node.parent.data >= node.data:
-                node.parent.left = s
-                s.left = node.left
-                s.right = node.right
-            else:
-                node.parent.right = s
-                s.left = node.left
-                s.right = node.right
+            print(t.data)
+            t.left = node.left
+            t.right = node.right
+            t.parent  = None
+            root = t
+        return root
 
 
-b = Binary_Search_Tree(50)
-b.insert_node(b,30,70,20,40,60,80,15,25,35,45,55,65,75,85)
+#b = Binary_Search_Tree(50)
+#b.insert_node(b,30,70,20,40,60,80,15,25,35,45,55,65,75,85)
+
+b = Binary_Search_Tree(5)
+b.insert_node(b,1,10,20,30,40,50,60,70)
 
 import tree_traversal, tree_operations
 print("\nInorder Traversal    = {}\n".format(tree_traversal.traversal('inorder', b)))
 print("\nPreorder Traversal   = {}\n".format(tree_traversal.traversal('preorder', b)))
 print("\nPostorder Traversal  = {}\n".format(tree_traversal.traversal('postorder', b)))
 
-#b.delete_node(b, 20)
+#c = b.delete_node(b, 50)
+c=b.delete_node(b, 5)
 
-print("\nNo. of Leaves in BST       = {}\n".format(tree_operations.no_of_leaves_bst(b)))
-print("\nNo. of Full node in BST    = {}\n".format(tree_operations.no_of_fullnode(b)))
-print("\nMaximum Height of BST      = {}\n".format(tree_operations.height_of_tree(b)))
+print("\nInorder Traversal    = {}\n".format(tree_traversal.traversal('inorder', c)))
+print("\nPreorder Traversal   = {}\n".format(tree_traversal.traversal('preorder', c)))
+print("\nPostorder Traversal  = {}\n".format(tree_traversal.traversal('postorder', c)))
+
+
+print("\nNo. of Leaves in BST       = {}\n".format(tree_operations.no_of_leaves_bst(c)))
+print("\nNo. of Full node in BST    = {}\n".format(tree_operations.no_of_fullnode(c)))
+print("\nMaximum Height of BST      = {}\n".format(tree_operations.height_of_tree(c)))
