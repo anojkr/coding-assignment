0058b59291adce0a1445897a5df532cfc8dcefc7
Anoj Kumar <anoj.kumar48@gmail.com>
2018-05-13 19:00:32 +0530

    Matrix chain multiplication

---
+++ dynamic_programming/matrix_chain_multiplication.py
@@ -0,0 +1,29 @@
+#Matrix-chain-multiplication {Reference cormen book- Dynamic programming}
+
+def matrix_chain_mul(matrix_order):
+	#alist.insert(0,'dummy')
+	n = len(matrix_order)
+	table = [[ int(1) for x in range(n)] for y in range(n)]
+	s = [[ int(0) for x in range(n)] for y in range(n)]
+
+	for i in range(1, n):
+		table[i][i] = 0
+	
+	for l in range(2, n+1):
+		for i in range(1, n-l+1):
+			j= i+l-1
+			r = []
+			for k in range(i,j):
+				#print("{}-{}".format(i,j))
+				#print("{}-{}-{}".format(i-1,k,j))
+				q = table[i][k] + table[k+1][j] + matrix_order[i-1]*matrix_order[k]*matrix_order[j]
+				#print("{}--{}--{}*{}*{}\n".format(table[i][k], table[k+1][j],matrix_order[i-1],matrix_order[k],matrix_order[j] ))
+				r.append(q)
+				table[i][j] = min(r)
+	
+	[print(table[k]) for k in range(1,n-1)]
+	return table[1][n-1]
+
+
+a = [30,35,15,5,10,20,25]
+print(matrix_chain_mul(a))
--- dynamic_programming/subset_sum.py
+++ dynamic_programming/subset_sum.py
@@ -1,8 +1,8 @@
 def subset_sum(s, sum):
 	c = sum + 1
 	r = len(s)+1
-	
 	table = [[int(0) for x in range(c)] for y in range(r)]
+
 	for b in range(r):
 		for sumz in range(c):
 			if sumz==0:
@@ -13,7 +13,9 @@ def subset_sum(s, sum):
 				table[b][sumz] = table[b-1][sumz]
 			elif sumz>=s[b-1]:
 				table[b][sumz] = table[b-1][sumz-b] or table[b-1][sumz-s[b-1]]
+				
 	[print(k) for k in table]
+
 
 #a = [3, 34, 4, 12, 5, 2]
 a = [1, 2, 3]
